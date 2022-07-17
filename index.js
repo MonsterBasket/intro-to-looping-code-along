6dce5517b38d945c88636df8f3890a7dc6cc1558
MonsterBasket <blazza1@hotmail.com>
2022-07-17 15:31:08 +1000

    test complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+const { getModulesPluginNames } = require("@babel/preset-env");
+
+function writeCards(names){
+    let newArray = [];
+    for (let i = 0; i < names.length; i++){
+        newArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return newArray;
+}
+function countDown(num){
+    for (let i = num; i >= 0; i--){
+        console.log(i);
+    }
+}
